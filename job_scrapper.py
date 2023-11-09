d3d4afd43650e4db6dffddfd0ee9b18346af7b43
Ondřej Tuček <ondrejtucek9@gmail.com>
2023-11-09 09:42:10 +0100

    refactoring

--- job_scrapper.py
+++ job_scrapper.py
@@ -1,4 +1,8 @@
-URL = ""
+# root of the website: https://www.google.com/
+BASE_URL = ""
+
+# root + additional params: https://www.google.com/search?hl=en&q=ee
+FULL_URL = ""
 
 from bs4 import BeautifulSoup
 import requests
@@ -8,6 +12,9 @@ import os
 
 
 def get_soup(url: str) -> BeautifulSoup:
+    """A function, that takes a URL and converts it's HTML to soup.
+    Raises exception if HTTP code is not 200
+    """
     page = requests.get(url)
 
     if page.status_code != 200:
@@ -18,78 +25,118 @@ def get_soup(url: str) -> BeautifulSoup:
     return soup
 
 
-def get_titles_from_page(soup: BeautifulSoup) -> list:
-    titles = soup.find_all("a", class_="link-primary SearchResultCard__titleLink")
+def get_data_from_soup(soup: BeautifulSoup) -> list:
+    """A function that takes a soup and retrieves a required data"""
     results = []
+    titles = soup.find_all("a", class_="link-primary SearchResultCard__titleLink")
+
+    if titles is None or len(titles) == 0:
+        return results
 
     for title in titles:
-        title_text = title.text.strip()
-        link = title["href"]
+        title_text = title.text
+        title_link = title["href"]
 
-        results.append([title_text, link])
+        if title_text and title_link:
+            results.append([title_text.strip(), title_link])
 
     return results
 
 
-def get_pages(soup: BeautifulSoup) -> list | None:
+def get_paging(soup: BeautifulSoup) -> list | None:
+    """A function that takes a soup and finds all links to next pages"""
     pagination = soup.find_all("li", class_="Pagination__item")
-    pages = []
 
-    for p in pagination:
-        a = p.find("a")
-        href = a["href"]
-        if href not in pages:
-            pages.append(href)
-
-    if len(pages) == 0:
+    if pagination is None or len(pagination) == 0:
         return None
 
-    return pages
+    results = []
+
+    for p in pagination:
+        link = p.find("a")
+        href = link["href"]
+
+        if href not in results:
+            results.append(href)
+
+    return results
 
 
-def collect_titles(links: list | None) -> list:
-    BASE_URL = ""
+def collect_data(links: list | None) -> list:
+    """A function that takes list of links as parameter and for each link it searches for data"""
+    if links is None or len(links) == 0:
+        return []
+
     result = []
+    for link in links:
+        link_url = BASE_URL + link
 
-    iters = links
+        print(f"Fetching HTML from {link_url}\n")
 
-    for link in iters:
-        url = BASE_URL + link
-        print(url)
-        soup = get_soup(url)
-        titles = get_titles_from_page(soup)
+        soup = get_soup(link_url)
+        data = get_data_from_soup(soup)
 
+        # check for continuos pagination
         last_link = links[-1]
         if link == last_link:
-            new_links = get_pages(soup)
-            iters.extend([link for link in new_links if link not in iters])
+            new_links = get_paging(soup)
+            links.extend([link for link in new_links if link not in links])
 
-        result.extend(titles)
+        result.append(data)
 
-    return result
+    return data
+
+
+def save_as_csv(results: list) -> None:
+    """function that tries to save results to file"""
+    file_name = os.path.join(os.getcwd(), "jobs-" + str(time.time()) + ".csv")
+    csv_titles = ["TITLE", "LINK"]
+
+    try:
+        with open(file_name, "w", encoding="utf-8") as file:
+            writer = csv.writer(file)
+
+            writer.writerow(csv_titles)
+            writer.writerows(results)
+
+    except Exception as e:
+        print(f"Failed to save data as CSV, trying as TXT")
+        fail_path = os.path.join(os.getcwd(), "failed.txt")
+
+        try:
+            with open(fail_path, "w") as f:
+                f.writelines(results)
+        except Exception as e:
+            print("Even failed to save results as txt file...sorry")
+        else:
+            print(f"Results were saved in: {fail_path}")
+    else:
+        print(f"Results were saved in: {file_name}")
 
 
 def init() -> None:
     try:
         start_delta = time.perf_counter()
-        print(f"Gathering data from {URL}")
-        soup = get_soup(URL)
-        pages = get_pages(soup)
-        titles = collect_titles(pages)
-        output_location = os.path.join(os.getcwd(), "jobs-" + str(time.time()) + ".csv")
 
-        fields = ["Title", "Link"]
+        print(f"Gathering data from {BASE_URL}")
+        print(
+            "I advice you go and make yourself some beverage, since I am retard and concurrency is hard\n"
+        )
+        initial_soup = get_soup(FULL_URL)
+        initial_data = get_data_from_soup(initial_soup)
+        links = get_paging(initial_soup)
 
-        with open(output_location, "w", encoding="utf-8") as csvfile:
-            writer = csv.writer(csvfile)
+        if links is None:
+            data = initial_data
+        else:
+            data = collect_data([link for link in links if link != FULL_URL])
+            data.extend(initial_data)
 
-            writer.writerow(fields, delimiter=",")
-            writer.writerows(titles, delimiter=",")
+        save_as_csv(data)
 
         end_delta = time.perf_counter()
-        print(f"Final time: {end_delta - start_delta}")
-        print(f"Data saved at: {output_location}")
-
+        print("Finished")
+        print(f"Final time: {end_delta - start_delta}s")
     except Exception as e:
         print(
             f"\033[38;2;255;0;0mUnexpected error occurred:\n{e}\033[38;2;255;255;255m"
@@ -98,3 +145,5 @@ def init() -> None:
 
 if __name__ == "__main__":
     init()
+else:
+    print("This file is suppose to be run as a script")
